3aee44661e1e0fb5ddcd4984695783d372d6ecce
Michael Alex <michaelalex94536@gmail.com>
2024-04-30 12:52:06 -0700

    Initial commit

---
+++ BareMetalAdv/test_out/Debug/Src/main.d
@@ -0,0 +1,17 @@
+Src/main.o: ../Src/main.c ../Inc/stm32f4xx.h ../Inc/stm32f446xx.h \
+ ..//chip_headers/CMSIS/Include/core_cm4.h \
+ ..//chip_headers/CMSIS/Include/cmsis_version.h \
+ ..//chip_headers/CMSIS/Include/cmsis_compiler.h \
+ ..//chip_headers/CMSIS/Include/cmsis_gcc.h \
+ ..//chip_headers/CMSIS/Include/mpu_armv7.h ../Inc/system_stm32f4xx.h \
+ ../Inc/uart.h ../Inc/stm32f4xx.h
+../Inc/stm32f4xx.h:
+../Inc/stm32f446xx.h:
+..//chip_headers/CMSIS/Include/core_cm4.h:
+..//chip_headers/CMSIS/Include/cmsis_version.h:
+..//chip_headers/CMSIS/Include/cmsis_compiler.h:
+..//chip_headers/CMSIS/Include/cmsis_gcc.h:
+..//chip_headers/CMSIS/Include/mpu_armv7.h:
+../Inc/system_stm32f4xx.h:
+../Inc/uart.h:
+../Inc/stm32f4xx.h:
---
+++ BareMetalAdv/test_out/Debug/Src/uart.d
@@ -0,0 +1,15 @@
+Src/uart.o: ../Src/uart.c ../Inc/uart.h ../Inc/stm32f4xx.h \
+ ../Inc/stm32f446xx.h ..//chip_headers/CMSIS/Include/core_cm4.h \
+ ..//chip_headers/CMSIS/Include/cmsis_version.h \
+ ..//chip_headers/CMSIS/Include/cmsis_compiler.h \
+ ..//chip_headers/CMSIS/Include/cmsis_gcc.h \
+ ..//chip_headers/CMSIS/Include/mpu_armv7.h ../Inc/system_stm32f4xx.h
+../Inc/uart.h:
+../Inc/stm32f4xx.h:
+../Inc/stm32f446xx.h:
+..//chip_headers/CMSIS/Include/core_cm4.h:
+..//chip_headers/CMSIS/Include/cmsis_version.h:
+..//chip_headers/CMSIS/Include/cmsis_compiler.h:
+..//chip_headers/CMSIS/Include/cmsis_gcc.h:
+..//chip_headers/CMSIS/Include/mpu_armv7.h:
+../Inc/system_stm32f4xx.h:
